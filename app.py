710f1c9576b0bd66252c57a3e27afcd7f88a6f66
YAnisimov <fa-tum@yandex.ru>
2019-12-05 12:28:51 +0300

    added video page, alright

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ playlists = {
 
 videos = {
   0: {"id":"0", "title":"How It Works: Internet of Things", "videoid":"QSIPNhOiMoE", "tags":[0, 4]},
-  1: {"id":"1", "title":"Top 10 IoT(Internet Of Things) Projects Of All Time | 2018", "videoid":"QL-6PdiDTeo", "tags":[1, 2, 3]},
+  1: {"id":"1", "title":"Top 10 IoT(Internet Of Things) Projects Of All Time | 2018", "videoid":"QL-6PdiDTeo", "tags":[1, 2]},
   2: {"id":"2", "title":"Diy Door Status Notifier", "videoid":"7s0RQDi6wdo", "tags":[1, 5]},
   3: {"id":"3", "title":"Top 5 IoT Hardware Platforms", "videoid":"vZNwkPe3gyQ", "tags":[1, 5, 2]},
 }
@@ -36,7 +36,11 @@ def about():
 
 @app.route('/videos/<id>')
 def video(id):
-    output = render_template("video.html")
+    id_int = int(id)
+    tag_list = []
+    for tag_id in videos[id_int]["tags"]:
+        tag_list.append(tags[tag_id])
+    output = render_template("video.html", video_name=videos[id_int]["title"], tags=tag_list, videoid=videos[id_int]["videoid"])
     return output
 @app.errorhandler(404)
 def page_not_found(error):
